fade2564efed958e7faff6695abf0a8bd9fde626
jackjavi <jackmtembete@gmail.com>
2022-08-01 05:50:23 +0300

    code

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const survey = () => {
   return (
     <div className="bg-[#B4B4BB] h-screen w-screen px-8 py-4 flex flex-col items-center">
-      <div className="flex flex-col bg-white p-4 rounded-lg w-[75vw]">
+      <div className="flex flex-col bg-white p-4 rounded-lg w-[100%]">
         <label>What is your name?</label>
         <input type="text" className="rounded shadow border px-2 py-1" />
       </div>
 
-      <div className="bg-white p-4 rounded-lg w-[75vw] mt-2">
+      <div className="bg-white p-4 rounded-lg w-[100%] mt-2">
         <p className="text-black">
           Choose your favorite social media platform?
         </p>
@@ -37,7 +37,7 @@ const survey = () => {
         </label>
       </div>
 
-      <div className="bg-white p-4 rounded-lg w-[75vw] mt-2">
+      <div className="bg-white p-4 rounded-lg w-[100%] mt-2">
         <p className="text-black">What do you mostly use social media for?</p>
 
         <input
