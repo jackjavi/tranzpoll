fc2bc4daa0bfbbceb68ee9404c6822c78fc4ccc3
jackjavi <jackmtembete@gmail.com>
2022-08-13 08:09:38 +0300

    refactored code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,8 +12,9 @@ const Login = () => {
           <div className="flex items-center gap-1 ">
             <span>+254</span>
             <input
-              className="border-2 border-black rounded-lg p-1"
               type="number"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
             />
           </div>
           <p>
