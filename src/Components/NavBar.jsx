65f6a5e0211b52b0c3e2f5cf83ede547e9861ee3
jackjavi <jackmtembete@gmail.com>
2022-08-16 21:26:01 +0300

    refactored code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className="flex justify-center items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-xl md:text-3xl font-signature ml-2 ">
-          TRANZPOLL COMMUNICATIONS
+          TRANSPOLL SURVEYS
         </h1>
       </div>
     </div>
