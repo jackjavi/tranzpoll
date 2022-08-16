71f7d4a6f799119890a70617e3dbfebf85767096
jackjavi <jackmtembete@gmail.com>
2022-08-16 23:26:55 +0300

    refactored code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="drop-shadow-lg flex justify-center items-center w-full h-20  px-4 text-white font-[900] bg-cyan-600 fixed">
-      <div className="grid grid-cols-2 gap-1 w-screen px-8 ">
-        <span className="text-[30px]">
+      <div className="flex  w-screen px-8 ">
+        <span className="text-[30px] w-[40%]">
           <span className="bg-gradient-to-r from-cyan-500 to-blue-500">T</span>
           <span className="text-[#1877f2] bg-gradient-to-r from-cyan-500 to-blue-500">
             S
           </span>
         </span>
-        <h1 className="font-sans whitespace-nowrap flex items-center no-wrap text-md md:text-xl md:text-3xl font-signature ml-2 ml-[-25%] ">
+        <h1 className="font-sans whitespace-nowrap flex items-center no-wrap text-md md:text-xl md:text-3xl font-signature  ">
           TRANSPOLL SURVEYS
         </h1>
       </div>
