3151e657f081192616b0a987adbfa6eb7743a672
jackjavi <jackmtembete@gmail.com>
2022-08-16 12:52:32 +0300

    refactored code

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -6,7 +6,13 @@ const survey = () => {
   return (
     <>
       <NavBar />
-      <div className=" h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500 px-8 pt-20 flex flex-col items-center justify-center"></div>
+      <div className=" h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500 px-8 pt-20 flex flex-col items-center justify-center">
+        <div className="text-white flex flex-col ">
+          <h3>POLL COMPLETE!</h3>
+          <p>THANK YOU FOR YOUR SUBMISSION</p>
+          <p>YOU WILL RECEIVE YOUR PAYMENT SOON</p>
+        </div>
+      </div>
     </>
   );
 };
