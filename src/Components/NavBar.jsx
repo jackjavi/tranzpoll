379c9425a6a5718373fcf9a0d72dff6f65944f1a
jackjavi <jackmtembete@gmail.com>
2022-08-16 23:29:08 +0300

    refactored code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="drop-shadow-lg flex justify-center items-center w-full h-20  px-4 text-white font-[900] bg-cyan-600 fixed">
       <div className="flex  w-screen px-8 ">
-        <span className="text-[30px] w-[40%]">
+        <span className="text-[30px] w-[20%] md:w-[40%]">
           <span className="bg-gradient-to-r from-cyan-500 to-blue-500">T</span>
           <span className="text-[#1877f2] bg-gradient-to-r from-cyan-500 to-blue-500">
             S
