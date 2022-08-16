4667bc8dfeb28623c51230838c05f5ec48140a2a
jackjavi <jackmtembete@gmail.com>
2022-08-16 23:19:05 +0300

    refactored code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,9 +6,11 @@ const NavBar = () => {
       <div className="grid grid-cols-2 gap-1 w-screen px-8 ">
         <span className="text-[30px]">
           <span className="bg-gradient-to-r from-cyan-500 to-blue-500">T</span>
-          <span className="text-[#1877f2]">S</span>
+          <span className="text-[#1877f2] bg-gradient-to-r from-cyan-500 to-blue-500">
+            S
+          </span>
         </span>
-        <h1 className=" text-md md:text-xl md:text-3xl font-signature ml-2 ml-[-25%] ">
+        <h1 className="font-sans flex items-center text-md md:text-xl md:text-3xl font-signature ml-2 ml-[-25%] ">
           TRANSPOLL SURVEYS
         </h1>
       </div>
