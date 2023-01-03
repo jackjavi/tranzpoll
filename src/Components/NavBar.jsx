d70c66e6824d2e4b4b64a1fd7ee8ce413712187d
jackjavi <jackmtembete@gmail.com>
2023-01-03 11:00:17 +0300

    Linked logo to home

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="drop-shadow-lg flex justify-center items-center w-full h-20  px-4 text-white font-[900] bg-cyan-600 fixed">
       <div className="flex  w-screen px-8 ">
-        <span className="text-[30px] w-[20%] md:w-[40%]">
-          <span className="bg-gradient-to-r from-cyan-500 to-blue-500">T</span>
-          <span className="text-[#1877f2] bg-gradient-to-r from-cyan-500 to-blue-500">
-            S
+        <Link to="/">
+          <span className="text-[30px] w-[20%] md:w-[40%] cursor-pointer">
+            <span className="bg-gradient-to-r from-cyan-500 to-blue-500">
+              T
+            </span>
+            <span className="text-[#1877f2] bg-gradient-to-r from-cyan-500 to-blue-500">
+              S
+            </span>
           </span>
-        </span>
-        <h1 className="font-sans whitespace-nowrap flex items-center no-wrap text-md md:text-xl md:text-3xl font-signature  ">
+        </Link>
+
+        <h1 className="font-sans mx-auto whitespace-nowrap flex items-center no-wrap text-md md:text-xl md:text-3xl  ">
           TRANSPOLL SURVEYS
         </h1>
       </div>
