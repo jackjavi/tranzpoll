53afd2c5c2900438f39eba5c4e63228eac1b2f81
jackjavi <jackmtembete@gmail.com>
2023-01-30 14:31:44 +0300

    Added paypal

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
+import mpesa from "../Assets/mpesa.png";
+import paypal from "../Assets/PayPal.svg.png";
+import Separator from "../Components/Separator";
 
 const Login = () => {
   const [faveMedia, setFaveMedia] = React.useState(null);
@@ -19,17 +22,46 @@ const Login = () => {
     <>
       <NavBar />
       <div className=" h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500 px-8 pt-20 flex flex-col items-center justify-center">
-        <form onSubmit={handleFaveMedia} className="bg-white p-4 w-[50vw]">
-          <p className="font-bold text-[14px]">ENTER YOUR MOBILE NUMBER:</p>
-          <div className="flex items-center gap-1 mt-4">
-            <span>+254</span>
-            <input
-              type="number"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-              name="number"
-            />
+        <form onSubmit={handleFaveMedia} className="bg-white p-4 w-[75vw]">
+          <div>
+            <p className="font-bold text-[14px]">ENTER YOUR MOBILE NUMBER:</p>
+            <div className="flex flex-col items-center  ">
+              <img
+                src={mpesa}
+                height=""
+                width="50px"
+                alt="Instagram logo"
+                className=""
+              />
+
+              <input
+                type="number"
+                placeholder="e.g. +254 7xxxxxxxx"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                name="number"
+              />
+            </div>
           </div>
+          <Separator />
+          <div>
+            <p className="font-bold text-[14px]">ENTER YOUR PAYPAL ACCOUNT:</p>
+            <div className="flex flex-col items-center  ">
+              <img
+                src={paypal}
+                height=""
+                width="50px"
+                alt="Instagram logo"
+                className="mb-1"
+              />
+
+              <input
+                type="number"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                name="number"
+              />
+            </div>
+          </div>
+
           <p className="mt-8 font-medium">
             Click
             <button
